3223616bbce139956d52b36309f60cbd1b0a3137
zhouxinze <xinze.zhou@hipac.cn>
2021-06-08 13:57:04 +0800

    spring IOC 加载流程（一）

---
+++ src/main/java/com/example/blog/jvm/Math.java
@@ -0,0 +1,14 @@
+package com.example.blog.jvm;
+
+public class Math {
+
+    public static void main(String[] args) {
+        Math math = new Math();
+        math.say();
+    }
+
+    private void say() {
+        System.out.println("Hello World");
+    }
+
+}
